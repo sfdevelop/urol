cc519c2d56184924b47ea5b2425d100f3621de46
sfdevelop <sfdevelop747@gmail.com>
2021-09-21 00:45:44 +0300

    Initial commit

---
+++ src/assets/js/custom/sfdevelop.js
@@ -0,0 +1,123 @@
+$(document).on('click', '.dropdown-menu', function (e) {
+    e.stopPropagation();
+});
+
+// make it as accordion for smaller screens
+if ($(window).width() < 992) {
+    $('.dropdown-menu a').click(function(e){
+        e.preventDefault();
+        if($(this).next('.submenu').length){
+            $(this).next('.submenu').toggle();
+        }
+        $('.dropdown').on('hide.bs.dropdown', function () {
+            $(this).find('.submenu').hide();
+        })
+    });
+}
+
+// основной слайдер
+$(document).ready(function () {
+    $('.slider-img').slick({
+        dots: true,
+        infinite: true,
+        speed: 700,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 6000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows:0,
+        arrows:false,
+    });
+});
+
+$('.new_slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    rows:0,
+    arrows:true,
+    responsive: [
+        {
+            breakpoint: 1600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 785,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                rows:0,
+            }
+        },
+    ]
+});
+
+$('.sale_slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    rows:0,
+    arrows:true,
+    responsive: [
+        {
+            breakpoint: 1600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 785,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                rows:0,
+            }
+        },
+    ]
+});
+
+$('.news_slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    rows:0,
+    arrows:false,
+    responsive: [
+        {
+            breakpoint: 1600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 785,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                rows:0,
+            }
+        },
+    ]
+});
---
+++ src/assets/js/main.js
@@ -0,0 +1,9 @@
+//= ../../../bower_components/jQuery/dist/jquery.js
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+
+//= ../../../bower_components/slick-carousel/slick/slick.js
+
+//= custom/sfdevelop.js
